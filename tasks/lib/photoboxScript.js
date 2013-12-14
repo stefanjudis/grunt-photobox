a5219888b5a9d0ea7f345249c0e6d3430437dbb5
stefan judis <stefanjudis@gmail.com>
2013-12-14 19:28:33 +0100

    added dynamic height support for image generation and diff generation

--- tasks/lib/photoboxScript.js
+++ tasks/lib/photoboxScript.js
@@ -6,6 +6,8 @@
  * Licensed under the MIT license.
  */
 
+
+
 var system        = require ( 'system' ),
     webpage       = require( 'webpage' ),
     fs            = require( 'fs' ),
@@ -13,9 +15,7 @@ var system        = require ( 'system' ),
     picture       = system.args[ 1 ],
     split         = picture.split( '|' ),
     url           = split[ 0 ],
-    size          = split[ 1 ].split( 'x' ),
-    width         = +size[ 0 ],
-    height        = +size[ 1 ],
+    width         = +split[ 1 ],
     indexPath     = system.args[ 2 ],
     settings      = fs.read( indexPath + 'options.json' );
 
@@ -33,29 +33,39 @@ if ( settings !== '{}' ) {
 }
 
 page.onError = function ( msg ) {
-    system.stderr.writeLine( 'ERROR:' + msg );
+  system.stderr.writeLine( 'ERROR:' + msg );
 };
 
 page.onConsoleMessage = function( msg, lineNum, sourceId ) {
-    system.stderr.writeLine( 'CONSOLE: ' + msg, lineNum, sourceId );
+  system.stderr.writeLine( 'CONSOLE: ' + msg, lineNum, sourceId );
 };
 
 page.viewportSize = {
-    height : height,
-    width  : width
-};
-
-page.clipRect = {
-  height : height,
+  height : 1000,
   width  : width
 };
 
 page.open( url, function( status ) {
   window.setTimeout( function() {
+    var height = page.evaluate( function() {
+       return Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+    } );
+
+    page.clipRect = {
+      top    : 0,
+      left   : 0,
+      height : height,
+      width  : width
+    };
+
     var imgPath = indexPath +
                     'img/current/' +
                     url.replace( /(http:\/\/|https:\/\/)/, '').replace( /\//g, '-') +
-                    '-' + width + 'x' + height +
+                    '-' + width +
                     '.png';
 
     console.log( 'Rendering ' + imgPath );
