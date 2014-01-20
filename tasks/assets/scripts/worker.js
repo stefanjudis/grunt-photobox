3b6d31a5a3d9f9b48cbc48a3548b281b8a47131c
haeringer <christian@haeringer.net>
2014-01-20 21:47:09 +0100

    added filter (brighter, darker and grayscale) to diff canvas

--- tasks/assets/scripts/worker.js
+++ tasks/assets/scripts/worker.js
@@ -49,6 +49,7 @@ function diff( pixelsA, pixelsB, config ) {
       threshold  = ~~config.threshold,
       color      = getHighlightColor ( config.higlightColor ),
       adjustment = 150,
+      filter     = 'brighter',
       i;
 
 
@@ -64,9 +65,23 @@ function diff( pixelsA, pixelsB, config ) {
       pixelsC.data[ i + 2 ] = color[ 2 ]; // b
       diffAmount++;
     } else {
-      pixelsC.data[ i ]     += adjustment; // r
-      pixelsC.data[ i + 1 ] += adjustment; // g
-      pixelsC.data[ i + 2 ] += adjustment; // b
+      // no difference found
+      if ( filter === 'brighter' ) {
+        pixelsC.data[ i ]     += adjustment; // r
+        pixelsC.data[ i + 1 ] += adjustment; // g
+        pixelsC.data[ i + 2 ] += adjustment; // b
+      } else if ( filter === 'darker' ) {
+        pixelsC.data[ i ]     -= adjustment; // r
+        pixelsC.data[ i + 1 ] -= adjustment; // g
+        pixelsC.data[ i + 2 ] -= adjustment; // b
+      } else if ( filter === 'grayscale' ) {
+
+        var y = 0.2126 * pixelsC.data[ i ] +
+                0.7152 * pixelsC.data[ i + 1 ] +
+                0.0722 * pixelsC.data[ i + 2 ];
+
+        pixelsC.data[ i ] = pixelsC.data[ i + 1 ] = pixelsC.data[ i + 2 ] = y;
+      }
     }
   }
 
