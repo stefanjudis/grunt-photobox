5eace63a6a4758e2fd22bd52ce0480dc1718d785
Daniel Husar <dano.husar@gmail.com>
2015-01-18 21:48:50 +0000

    use filenamify for save paths

--- tasks/lib/photobox.js
+++ tasks/lib/photobox.js
@@ -10,6 +10,7 @@
 
 var fs           = require( 'fs' ),
     path         = require( 'path' ),
+    filenamify   = require('filenamify'),
     phantomjs    = require( 'phantomjs' ),
     phantomPath  = phantomjs.path;
 
@@ -508,12 +509,12 @@ PhotoBox.prototype.startPhotoSession = function() {
     var parsedImage = require( 'url' ).parse(picture);
     var finalImage = ((!this.options.relativePaths ? parsedImage.host + '/' : '') +
                    (parsedImage.path !== '/' ? parsedImage.path : 'index').replace(/^\//, '') +
-                   (parsedImage.query ? '?' + parsedImage.query : '')).replace( /^www\./g, '').replace( /\//g, '-');
+                   (parsedImage.query ? '?' + parsedImage.query : '')).replace( /^www\./g, '');
 
     var args = [
       '--ssl-protocol=any',
       path.resolve( __dirname, 'photoboxScript.js' ),
-      picture + '#' + finalImage,
+      picture + '#' + filenamify(finalImage, {replacement: '-'}),
       this.options.indexPath,
       this.options.indexPath + 'options.json'
     ];
